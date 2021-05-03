8dc4888c43f2a667a3b9dc380b354adb96fa26a1
Brandon <brandoncao74@gmail.com>
2021-05-03 02:02:26 -0400

    add route for questions forum to display all questions asked

--- routes/questions-forum.js
+++ routes/questions-forum.js
@@ -1,104 +1,134 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const xss = require('xss');
-const questionData = require('../data/questions');
-const userData = require('../data/users');
+const xss = require("xss");
+const questionData = require("../data/questions");
+const userData = require("../data/users");
 
 function createHelper(string) {
-    if(string === undefined || typeof string !== 'string' || string.trim().length === 0) {
-        return false;
-    } else {
-        return true;
-    }
+	if (string === undefined || typeof string !== "string" || string.trim().length === 0) {
+		return false;
+	} else {
+		return true;
+	}
 }
 
 // Route to display all questions asked
-router.get('/', async (req, res) => {
-    let questionList = await questionData.getQuestions();
-})
+router.get("/", async (req, res) => {
+	let questionList = await questionData.getQuestions();
+	return res.render("questions/questions-forum", {
+		title: "Question Forum",
+		loggedIn: !!req.session.user,
+		questions: questionList,
+	});
+});
+
+// TODO: Route to display single question asked
+// router.get("/:id", async (req, res) => {
+	
+// });
 
 // Route to ask a new question page
-router.get('/post',async (req,res) => {
+router.get("/post", async (req, res) => {
 	if (req.session.user) {
-		return res.render('questions/ask-question',{title:"Ask a Question", loggedIn: true});
- 	}
-	 res.redirect('/login');
+		return res.render("questions/ask-question", {
+			title: "Ask a Question",
+			loggedIn: true,
+		});
+	}
+	res.redirect("/login");
 });
 
 // Route for posting a new question
-router.post('/post', async (req,res) => {
-	let questionInfo = xss(req.body);
-	let title = xss(req.body.questionTitle);
-    let questionBody = xss(req.body.questionBody);
-    let tags = xss(req.body.questionTags);
+router.post("/post", async (req, res) => {
+	if (req.session.user) {
+		let questionInfo = xss(req.body);
+		let title = xss(req.body.questionTitle);
+		let questionBody = xss(req.body.questionBody);
+		let tags = xss(req.body.questionTags);
 
-	if (!questionInfo) {
-		res.status(400).json({error: "You must provide data to ask a question"});
-		return;
-	}
+		if (!questionInfo) {
+			res.status(400).json({
+				error: "You must provide data to ask a question",
+			});
+			return;
+		}
 
-	if(createHelper(title) === false) {
-		res.status(400).json({ error: "Error: Title must be provided and proper type" });
-		return;
-	}
+		if (createHelper(title) === false) {
+			res.status(400).json({
+				error: "Error: Title must be provided and proper type",
+			});
+			return;
+		}
 
-	if(createHelper(questionBody) === false) {
-		res.status(400).json({ error: "Error: Question body must be provided and proper type" });
-		return;
-	}
+		if (createHelper(questionBody) === false) {
+			res.status(400).json({
+				error: "Error: Question body must be provided and proper type",
+			});
+			return;
+		}
 
-	if(createHelper(tags) === false) {
-		res.status(400).json({ error: "Error: Tag(s) must be provided and proper type" });
-		return;
-	}
-	
-	if(createHelper(tags) === true) {
-		tags = tags.split(',');
-		for(let i = 0; i < tags.length; i++) {
-			tags[i] = tags[i].trim();
-            if(createHelper(tags[i]) === false) {
-				res.status(400).json({ error: "Error: Each tag must be a string and non all empty space string" });
+		if (createHelper(tags) === false) {
+			res.status(400).json({
+				error: "Error: Tag(s) must be provided and proper type",
+			});
+			return;
+		}
+
+		if (createHelper(tags) === true) {
+			tags = tags.split(",");
+			for (let i = 0; i < tags.length; i++) {
+				tags[i] = tags[i].trim();
+				if (createHelper(tags[i]) === false) {
+					res.status(400).json({
+						error:
+							"Error: Each tag must be a string and non all empty space string",
+					});
+				}
 			}
 		}
-	}
 
-	if (!title) {
-		res.status(400).json({ error: "You must provide a title" });
-		return;
-	}
-
-	if (!questionBody) {
-		res.status(400).json({ error: "You must provide a question body" });
-		return;
-	}
-
-	if (!tags) {
-		res.status(400).json({ error: "You must provide atleast one tag" });
-		return;
-	}
-
-	var regex = /(<([^>]+)>)/ig;
-    title = title.replace(regex, "");
-	questionBody = questionBody.replace(regex, "");
-	for(let i = 0; i < tags.length; i++) {
-		tags[i] = tags[i].replace(regex, "");
-	}
-
-	let currentUser = await userData.getUserByUsername(req.session.user.username);
-	let currentUserId = currentUser._id;
-
-	try {
-		const newQuestion = await questionData.createQuestion(
-			currentUserId,
-			title,
-			questionBody,
-			tags
-		);
-		if(newQuestion) {
-			res.status(201).json({message:'success'});
+		if (!title) {
+			res.status(400).json({ error: "You must provide a title" });
+			return;
 		}
-	} catch (e) {
-		res.status(500).json({ error: e });
+
+		if (!questionBody) {
+			res.status(400).json({ error: "You must provide a question body" });
+			return;
+		}
+
+		if (!tags) {
+			res.status(400).json({ error: "You must provide atleast one tag" });
+			return;
+		}
+
+		var regex = /(<([^>]+)>)/gi;
+		title = title.replace(regex, "");
+		questionBody = questionBody.replace(regex, "");
+		for (let i = 0; i < tags.length; i++) {
+			tags[i] = tags[i].replace(regex, "");
+		}
+
+		let currentUser = await userData.getUserByUsername(
+			req.session.user.username
+		);
+		let currentUserId = currentUser._id;
+
+		try {
+			const newQuestion = await questionData.createQuestion(
+				currentUserId,
+				title,
+				questionBody,
+				tags
+			);
+			if (newQuestion) {
+				res.status(201).json({ message: "success" });
+			}
+		} catch (e) {
+			res.status(500).json({ error: e });
+		}
+	} else {
+		res.sendStatus(403);
 	}
 });
 
